adab700769b068be3492889b6b193c904334fdfb
Oleg <oleg.pav.m@gmail.com>
2018-08-02 14:10:02 +0300

    Subscribe model.

---
+++ src/model/Subscribe.js
@@ -0,0 +1,82 @@
+const core = require('gls-core-service');
+const MongoDB = core.service.MongoDB;
+
+module.exports = MongoDB.makeModel(
+    'Subscribe',
+    {
+        user: {
+            type: String,
+            required: true,
+        },
+        online: {
+            type: Boolean,
+            default: false,
+        },
+        show: {
+            vote: {
+                type: Boolean,
+                default: true,
+            },
+            flag: {
+                type: Boolean,
+                default: true,
+            },
+            transfer: {
+                type: Boolean,
+                default: true,
+            },
+            reply: {
+                type: Boolean,
+                default: true,
+            },
+            subscribe: {
+                type: Boolean,
+                default: true,
+            },
+            unsubscribe: {
+                type: Boolean,
+                default: true,
+            },
+            mention: {
+                type: Boolean,
+                default: true,
+            },
+            repost: {
+                type: Boolean,
+                default: true,
+            },
+            award: {
+                type: Boolean,
+                default: true,
+            },
+            curatorAward: {
+                type: Boolean,
+                default: true,
+            },
+            message: {
+                type: Boolean,
+                default: true,
+            },
+            witnessVote: {
+                type: Boolean,
+                default: true,
+            },
+            witnessCancelVote: {
+                type: Boolean,
+                default: true,
+            },
+        },
+    },
+    {
+        index: [
+            {
+                fields: {
+                    user: 1,
+                },
+                options: {
+                    unique: true,
+                },
+            },
+        ],
+    }
+);
